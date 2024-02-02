eef3c1055dddc282b3e7adbf2e3d647804afd31e
Young-Yoii <fhqht4545@gmail.com>
2024-02-02 17:45:00 +0900

    FIX: db 변경

--- src/db.ts
+++ src/db.ts
@@ -24,28 +24,6 @@ interface experiecetype {
 export const project: projectType[] = [
   {
     id: 1,
-    title: '블로그 썸네일 메이커',
-    link: 'thumbnail',
-    image: '/images/thumbnail.png',
-    video: 'https://youtu.be/2hfo5xujF_s',
-    github: 'https://github.com/Young-Yoii/thumbnail-maker',
-    url: 'https://young-yoii.github.io/thumbnail-maker/',
-    date: '2023.01.16 - 2023.01.24',
-    category: '개인프로젝트',
-    overview:
-      '블로그 썸네일 하나 제작하기 위해서 미리캔버스, 온라인 포토샵, 어도비 포토샵 등의 툴을 사용하며\n하나 하나 만들기가 힘들기도 하였고, 무엇보다 썸네일 제작에 소요되는 시간이 아까웠습니다.\n토이프로젝트 주제를 생각하다 `평소 불편한 것들을 개선해보자!` 라는 생각에서 시작된\n블로그 썸네일을 간단하게 만들어주는 앱, 블로그 썸네일 메이커 입니다.',
-    role: null,
-    members: '1명',
-    description: ['이미지 다운로드 기능 구현', '클립보드 복사 기능 구현', '클립보드 복사시 토스트 메세지 구현', 'Redux-toolkit을 사용한 상태관리'],
-    review: [
-      'Redux-toolkit을 활용하여 상태관리 방법을 학습하고 적용함',
-      'useReducer와 ContextAPI를 사용하여 초기 구현한 뒤 redux-toolkit를 사용하여 리팩토링을 해봄',
-      'TypeSctipt의 타입 에러에 대해 공부하고 해결방법을 기록함',
-    ],
-    stack: ['React', 'TypeScript', 'Redux-toolkit'],
-  },
-  {
-    id: 2,
     title: '나도 땀',
     link: 'nadoddam',
     image: '/images/nadoddam.png',
@@ -54,7 +32,7 @@ export const project: projectType[] = [
     url: 'https://www.youtube.com/embed/RTndl4Qn7Zg',
     date: '2022.12.12 - 2022.12.28',
     overview:
-      '과일을 땀 흘려 수확하고 농부가 되어보자! \n풍년으로 인한 과일값 하락과 인력 부족으로 수확을 포기하는 농민들이 늘고 있다는 뉴스를 보고\n직접 수확하고 과일을 가져가는 체험 상품을 판매한다면 농민과 소비자 모두 이득이지 않을까 생각했습니다.\n또한 농장 체험을 예약하는 서비스가 부재하기 때문에 만든다면 수요가 있을 것이라 판단했습니다.\n그렇게 "나도땀" 은 농민과 소비자의 상생을 목표로 시작되었습니다!',
+      'React를 배우며 진행한 팀 프로젝트로 등록, 예약, 리뷰쓰기가 가능한 농장체험 신청 플랫폼입니다. ',
     role: 'front-end',
     members: 'FE 5명 / BE 2명',
     category: '팀 프로젝트',
@@ -75,7 +53,7 @@ export const project: projectType[] = [
     ],
   },
   {
-    id: 3,
+    id: 2,
     title: 'YFLIX',
     link: 'yflix',
     image: '/images/yflix.png',
@@ -84,7 +62,7 @@ export const project: projectType[] = [
     url: 'https://young-yoii.github.io/yflix/',
     category: '개인 프로젝트',
     date: '2022.11.21 - 2022.12.01',
-    overview: '엘리스 sw 트랙 중 진행한 스터디 활동에서 React 기초를 다지기 위해 제작한 영화 소개 페이지 입니다.',
+    overview: 'React를 배우며 진행한 개인 프로젝트로 제작한 영화 소개 페이지 입니다.',
     role: null,
     members: '1명',
     review: ['오픈 API를 사용하여 서버와 통신하는 로직을 이해하고 적용함', 'react-hooks를 사용하는 방법을 배우고 적용함', 'react-router를 사용하는 방법을 배우고 적용함'],
@@ -92,7 +70,7 @@ export const project: projectType[] = [
     stack: ['React', 'JavaScript', 'css-module'],
   },
   {
-    id: 4,
+    id: 3,
     title: '$merge cake',
     link: 'mergecake',
     image: '/images/merge.png',
@@ -100,7 +78,7 @@ export const project: projectType[] = [
     github: 'https://github.com/Young-Yoii/MergeCake-team-project1',
     url: 'https://www.youtube.com/embed/BKXyOWmD79U',
     overview:
-      '주문 제작 케이크의 수요가 증가하고 있지만 여전히 SNS만을 사용하여 주문 제작을 해야합니다.\nSNS를 하지 않거나 간편하게 주문을 하고 싶은 사람들은 케이크를 주문 제작 하는 것을 포기하곤 합니다.\n케이크를 주문 제작하는 것에 어려움을 느끼는 분들을 위해 프로젝트를 시작했습니다.\n$merge cake는 간편한 방법으로 원하는 모양으로 케이크를 만들고 장바구니에 추가하고, \n또 주문을 할 수 있는 케이크 주문제작 쇼핑몰 입니다.',
+      '바닐라 자바스크립트를 이용하여 제작한 웹 페이지로 케이크 주문, 관리자 관리기능이 있는 프로젝트',
     role: 'front-end',
     members: 'FE 3명 / BE 2명',
     description: [
@@ -121,7 +99,7 @@ export const project: projectType[] = [
     stack: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    id: 5,
+    id: 4,
     title: 'Blend',
     link: 'blend',
     image: '/images/blend.png',
@@ -131,7 +109,7 @@ export const project: projectType[] = [
     date: '2021.10',
     category: '퍼블리싱',
     overview:
-      '2021년에 제작한 웹사이트 H&B스토어 쇼핑몰 BLEND 입니다.\n웹사이트 디자인 및 전체를 구축하였고,\n메인화면과 4개의 서브화면으로 구성되어있으며 Aos플로그인을 이용한 애니메이션과 슬라이더 등으로 동적인 웹사이트입니다. PC용으로만 제작되었습니다.',
+      'Aos플로그인을 이용한 애니메이션과 슬라이더 등으로 제작한 동적인 웹사이트',
     role: null,
     members: '1명',
     review: ['와이어 프레임을 만들고 그에따라 홈페이지를 제작함', 'HTML과 CSS를 활용하여 정적인 홈페이지를 만드는 방법을 깨달음'],
@@ -139,7 +117,7 @@ export const project: projectType[] = [
     stack: ['HTML', 'CSS', 'jQuery'],
   },
   {
-    id: 6,
+    id: 5,
     title: '선셋 맥주',
     link: 'sunsetbeer',
     image: '/images/senset.png',
@@ -148,21 +126,43 @@ export const project: projectType[] = [
     url: 'https://young-yoii.github.io/sunset-beer/',
     date: '2021.10',
     category: '퍼블리싱',
-    overview: '2021 년에 제작한 반응형 웹사이트 입니다. 웹사이트 디자인 및 전체를 구축하였으며\n반응형 슬라이더, Aos플로그인을 사용하여 동적인 화면을 구현하였고 메인화면만 제작 되었습니다.',
+    overview: 'Aos플로그인을 사용하여 동적인 화면으로 구현한 반응형 웹 사이트',
     role: null,
     members: '1명',
     review: ['와이어 프레임을 만들고 그에 따라 홈페이지를 제작함', '미디어 쿼리를 사용하여 반응형 웹을 만드는 방법을 깨달음'],
     description: ['홈페이지 전 페이지 디자인 담당 및 퍼블리싱', '반응형 웹사이트로 구현', 'HTML, CSS, jQuery 환경 UI'],
     stack: ['HTML', 'CSS', 'jQuery'],
   },
+  {
+    id: 6,
+    title: '블로그 썸네일 메이커',
+    link: 'thumbnail',
+    image: '/images/thumbnail.png',
+    video: 'https://youtu.be/2hfo5xujF_s',
+    github: 'https://github.com/Young-Yoii/thumbnail-maker',
+    url: 'https://young-yoii.github.io/thumbnail-maker/',
+    date: '2023.01.16 - 2023.01.24',
+    category: '개인프로젝트',
+    overview:
+      'React, redux를 이용하여 토이프로젝트로 제작한 블로그 썸네일 메이커',
+    role: null,
+    members: '1명',
+    description: ['이미지 다운로드 기능 구현', '클립보드 복사 기능 구현', '클립보드 복사시 토스트 메세지 구현', 'Redux-toolkit을 사용한 상태관리'],
+    review: [
+      'Redux-toolkit을 활용하여 상태관리 방법을 학습하고 적용함',
+      'useReducer와 ContextAPI를 사용하여 초기 구현한 뒤 redux-toolkit를 사용하여 리팩토링을 해봄',
+      'TypeSctipt의 타입 에러에 대해 공부하고 해결방법을 기록함',
+    ],
+    stack: ['React', 'TypeScript', 'Redux-toolkit'],
+  },
 ];
 
 export const experiece: experiecetype[] = [
   {
     id: 1,
-    title: '엘리스 SW 엔지니어 트랙 3기',
-    date: '2022.09.12 - 2022.12.31',
-    description: '웹프론트엔드 중심으로 백엔드를 함께 배우는 개발자 양성 교육 과정',
+    title: '△△ 기업 (시스템 소프트웨어 개발)',
+    date: '2023.04.03 - 현재',
+    description: '웹사이트 프론트 개발 및 퍼블리싱',
   },
   {
     id: 2,
@@ -172,14 +172,14 @@ export const experiece: experiecetype[] = [
   },
   {
     id: 3,
-    title: '웹디자인 / 퍼블리셔 과정 수료',
-    date: '2021.06.24 - 2021.10.24',
-    description: '웹디자이너 및 퍼블리셔 양성 교육과정',
+    title: '엘리스 SW 엔지니어 트랙 3기',
+    date: '2022.09.12 - 2022.12.31',
+    description: '웹프론트엔드 중심으로 백엔드를 함께 배우는 개발자 양성 교육 과정',
   },
   {
     id: 4,
-    title: '계명문화대학교 유아교육학과 학사졸업',
-    date: '2017.03 - 2020.02',
-    description: '',
+    title: '웹디자인 / 퍼블리셔 과정 수료',
+    date: '2021.06.24 - 2021.10.24',
+    description: '웹디자이너 및 퍼블리셔 양성 교육과정',
   },
 ];
